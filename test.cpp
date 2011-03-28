013382924e3c0f6f89f557becbbc82820185fd7b
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2011-03-29 00:27:04 +0100

    Some channges

--- test.cpp
+++ test.cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 #define DEFAULT_STEP 0.001
 #define MAX_VELOCITY_CHANGE 0.001
+#define MASS 0.001
 
 struct vector2d {
     double x, y;
@@ -28,7 +29,6 @@ int main(int argc, char* argv[]) {
     
     unsigned double end_time = atoi(argv[1]);
     unsigned double time=0;
-    double force;
     unsigned int i, j;
     data2d data;
 
@@ -49,13 +49,41 @@ int main(int argc, char* argv[]) {
     }
 
     //Try solving things :)
+    vector2d on, by;
+    double force;
     while (time < end_time) {
         // Make a step here
         for (i=0; i<data.size(); i++) {
+            on.x = data[i][0];
+            on.y = data[i][1];
+            
             // Calculate acceleration from each particle
+            vector2d acceleration;
+            acceleration.x = 0;
+            acceleration.y = 0;
+
             for (j=0; j<data.size(); j++) {
+                by.x = data[j].[0];
+                by.y = data[j].[1];
                 
+                // Calculate force
+                vector2d force, acceleration;
+                force = get_force(on, by);
+                
+                // Add to acceleration of current particle
+                acceleration.x += force.x / MASS;
+                acceleration.y += force.y / MASS;
             }
+
+            // Add to velocity x,y
+            data[i][2] += acceleration.x * DEFAULT_STEP;
+            data[i][3] += acceleration.y * DEFAULT_STEP;
+
+            // Change position
+            data[i][0] += data[i][2] * DEFAULT_STEP;
+            date[i][1] += data[i][3] * DEFAULT_STEP;
+
+            time += DEFAULT_STEP;
         }
     }
 }
